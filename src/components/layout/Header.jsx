c2bc45e0873785959d81b9b6f9c953df42332035
LeonardoCHb <leonardohably@hotmail.com>
2020-05-27 18:45:06 -0300

    removendo react-bootstrap e implementando menu basico

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
+
 import { NavLink } from 'react-router-dom'
 
 function Header(){
-    return(
-        <nav className="navbar-light">
+    return (
+      <nav className=" navbar navbar-light navbar-expand-lg bg-light">
         <NavLink to="" className="navbar-brand">Pizzaria X</NavLink>
-        <div >
-            <ul className="navbar-nav ">
-                <li className="nav-item">
-                    <NavLink to="/" className="nav-NavLink">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/SeuPedido" className="nav-NavLink">Faca seu Pedido</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/Cardapio" className="nav-NavLink">Cardapio</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/Contatos" className="nav-NavLink">Contatos</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/SobreNos" className="nav-NavLink">Sobre Nos</NavLink>
-                </li>
-            </ul>
-        </div>
+
+           <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                      <NavLink to="/" className="nav-link">Home</NavLink>
+                  </li>
+                  <li className="nav-item ">
+                      <NavLink to="/SeuPedido" className="nav-link">Faca seu Pedido</NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/Cardapio" className="nav-link">Cardapio</NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/Contatos" className="nav-link">Contatos</NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/SobreNos" className="nav-link">Sobre Nos</NavLink>
+                  </li>
+              </ul>
+           </div>
+
     </nav>
     )
-}
+    }
 
 export default Header
