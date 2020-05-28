7a61db3522ec3073fb45559584de0ee4700b875b
LeonardoCHb <leonardohably@hotmail.com>
2020-05-28 14:18:05 -0300

    imgs do background slider

--- src/components/content/Home.jsx
+++ src/components/content/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import Body from '../layout/Body'
 import Header from '../layout/Header'
 
 function Home (){
     return(
-        <Header/>
+        <React.Fragment>
+            <Header/>
+            <Body/>
+        </React.Fragment>
     )
 }
 
--- src/components/layout/Body.jsx
+++ src/components/layout/Body.jsx
@@ -1,6 +1,14 @@
 import React from "react"
 
  function Body(){
-     return <main></main> 
+     return (
+
+        <main role="main" className="container mt-5 p-5">
+        <div className="stater-template">
+            <h1>A melhor Pizzaria do MUNDO!</h1>
+        </div>
+        </main>
+
+     )
 }
  export default Body
