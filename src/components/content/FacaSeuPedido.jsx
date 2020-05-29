eb3afeb1c9a8b58db9dd9531f7153384c7561611
caiolucasb <caio.lucas75@gmail.com>
2020-05-29 16:08:50 -0300

    criando funçao que criar componentes da lisa do cardapio

--- src/components/content/FacaSeuPedido.jsx
+++ src/components/content/FacaSeuPedido.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 
+//funçao que adiciona os items(li) do cardapio no compnente principal do faça seu pedido
+function PutTyperOfFood(props) {
+        return (
+            <li className="list-group-item d-flex flex-row">
+                <input type="checkbox" className=""/>
+                <div>
+                    <label>
+                        <h5>{props.name}</h5>
+                        <small class="form-text text-muted">{props.text}</small>
+                    </label>
+                </div>
+                <span className="">preço</span>
+            </li>
+        )
+        
+    }
+
 class SeuPedido extends React.Component {
     constructor() {
         super()
         this.state = {
 
         }
+        
     }
+    
     render() {
         return (
             <form>
                 <ul className="category-menu list-group">
                     <li className="category-items list-group-item" >
                         <ul id="category-item-01-pizza list-group">
-                            <li className="list-group-item"><h3>Pizzas</h3></li>
+                            <h3>Pizzas</h3>
+                                <PutTyperOfFood 
+                                type="p" 
+                                name="Calabresa" 
+                                text="é isso"/>
                         </ul>
                     </li>
                     <li className="category-items list-group-item" >
@@ -33,7 +56,7 @@ class SeuPedido extends React.Component {
                     </li>
                     <li className="category-items list-group-item" >
                         <ul id="category-item-04-deserts list-group">
-                            <li className="list-group-item"><h3>Sobremesas(Desests)</h3></li>
+                            <li className="list-group-item"><h3>Sobremesas(Deserts)</h3></li>
                         </ul>
                     </li>
                 </ul>
