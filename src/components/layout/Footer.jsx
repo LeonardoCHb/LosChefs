1efee47533f58fb9c76edbb92d8841d2fc013b6c
LeonardoCHb <leonardohably@hotmail.com>
2020-06-01 19:33:36 -0300

    fixing overflow bug

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,9 +3,14 @@ import React from "react"
  function Footer(){
      return (
      <footer className="page-footer font-small unique-color-dark">
-        <div Style="background-color: #6351ce">
+        <div Style="background-color: #3d3d3d">
             <div className="container">
-                <div className="row py-4 d-flex align-items-center"></div>
+                <div className="row py-4 d-flex align-items-center">
+
+                <div className="col-md-6 col-lg-5 text-center text-mdpleft mb-4 mb-md-0">
+                    <h6 className="mb-0">Acesse Nossas redes sociais para mais novidades!</h6>
+                </div>
+                </div>
             </div>
         </div>
      </footer>
