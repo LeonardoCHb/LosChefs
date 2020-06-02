5fbeb7f7d62abd61ad8da8ea9a9a1d730aa1d582
LeonardoCHb <leonardohably@hotmail.com>
2020-06-02 10:45:31 -0300

    footer nice

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,28 +2,30 @@ import React from "react"
 import { NavLink } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.min.css';
 
+import './styles/Footer.css';
+
 function Footer() {
     return (
-        <footer className="page-footer font-small unique-color-dark">
+        <footer className="page-footer font-small unique-color-dark" id="main_footer">
 
-            <div Style="background-color: black">
-                <div className="container">
+            <div Style="background-color: red">
+                <div className="container" id="social_footer">
                     <div className="row py-4 d-flex align-items-center">
 
-                        <div className="col-md-6 col-lg-5 text-center text-mdpleft mb-4 mb-md-0">
+                        <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
 
-                            <h6 className="mb-0 text-light">Acesse Nossas redes sociais para mais novidades!</h6>
+                            <h6 className="mb-0 text-light" id="footer_main">Acesse Nossas redes sociais para mais novidades!</h6>
 
                         </div>
 
-                        <div class="col-md-6 col-lg-7 text-center text-md-right">
+                        <div className="col-md-6 col-lg-7 text-center text-md-right">
 
                             <a className="fb-ic" href="https://facebook.com">
-                            <i className="fa fa-facebook-square mr-4"></i>
+                                <i className="fa fa-facebook-square mr-4" id="footer_text"></i>
                             </a>
 
                             <a className="ins-ic" href="https://instagram.com">
-                            <i class="fa fa-instagram"></i>
+                                <i className="fa fa-instagram" id="footer_text"></i>
                             </a>
 
 
@@ -32,72 +34,76 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="container text-center text-md-left mt-5">
+            <div className="container text-center text-md-left mt-5" >
+
                 <div className="row mt-3">
 
                     <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                        <h6 class=" font-weight-bold">Los Chefs</h6>
+                        <h6 id="footer_title">Los Chefs</h6>
 
-                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
+                        <hr className=" accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
 
-                        <p>blalbablalbalblblabllbalbalblablabla</p>
+                        <p id="footer_text">blalbablalbalblblabllbalbalblablabla</p>
 
                     </div>
 
                     <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
-                        <h6 class="text-uppercase font-weight-bold">FRANQUIAS</h6>
-                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
+                        <h6 id="footer_title">FRANQUIAS</h6>
+                        <hr className="deep-red accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
                         <p>
-                            <NavLink to="" className="footer-link">Los Chefs (SP)</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Los Chefs (SP)</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Los Chefs (PR)</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Los Chefs (PR)</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Los Chefs (RJ)</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Los Chefs (RJ)</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Los Chefs (AC)</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Los Chefs (AC)</NavLink>
                         </p>
                     </div>
 
-                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                        <h6 class="text-uppercase font-weight-bold">Links Úteis</h6>
-                        <hr class="deep-red accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
+                    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                        <h6 id="footer_title">Links Úteis</h6>
+
+                        <hr className="deep-red accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
                         <p>
-                            <NavLink to="" className="footer-link">Seja um Investido!</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Seja um Investido!</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Projetos Sociais</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Projetos Sociais</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Shipping Rate</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Shipping Rate</NavLink>
                         </p>
                         <p>
-                            <NavLink to="" className="footer-link">Podemos Ajudar?</NavLink>
+                            <NavLink to="" className="footer-link" id="footer_text">Podemos Ajudar?</NavLink>
                         </p>
 
                     </div>
                     <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
 
 
-                        <h6 className="text-uppercase font-weight-bold">Contact</h6>
-                        <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
-                        <p>
-                            <i className="fa fa-home mr-3"></i> Brasilia DF, Lago Sul, BR</p>
-                        <p>
-                            <i className="fa fa-envelope mr-3"></i> LosChefs@gmail.com</p>
-                        <p>
-                            <i className="fa fa-phone mr-3"></i> + 55 061 95489321</p>
-                        <p>
-                        <i className="fa fa-mobile mr-3"></i> + 55 061 95862314</p>
+                        <h6 id="footer_title">Contatos</h6>
+                        <hr className="deep-red accent-2 mb-4 mt-0 d-inline-block mx-auto" Style="width: 60px;" />
+
+                        <p id="footer_text"><i className="fa fa-home mr-3" id="footer_text"></i> Brasilia DF, Lago Sul, BR</p>
+
+                        <p id="footer_text"><i className="fa fa-envelope mr-3" id="footer_text"></i> LosChefs@gmail.com</p>
+
+                        <p id="footer_text"><i className="fa fa-phone mr-3" id="footer_text"></i> + 55 061 95489321</p>
+
+                        <p id="footer_text"><i className="fa fa-mobile mr-3" id="footer_text"></i> + 55 061 95862314</p>
 
                     </div>
 
-
                 </div>
             </div>
+
+            <div className="footer-copyright text-center py-3 " Style="background-color: red" id="footer_main"><h6>© 2020 Copyright:Los Chefs</h6></div>
         </footer>
     )
 }
