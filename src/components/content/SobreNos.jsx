0845e9c623b9e7829faefc594d9cef3df8304612
LeonardoCHb <leonardohably@hotmail.com>
2020-06-04 08:12:50 -0300

    adding timeline to 'sobre nos' and fixing navbar color

--- src/components/content/SobreNos.jsx
+++ src/components/content/SobreNos.jsx
@@ -4,7 +4,6 @@ import Header from "../layout/Header"
 import Footer from "../layout/Footer"
 
 import TL_01 from "../img/TL_01.jpg"
-import TL_02 from "../img/TL_02.jpg"
 import TL_03 from "../img/TL_03.jpg"
 import TL_04 from "../img/TL_4.jpg"
 import TL_05 from "../img/TL_5.jpg"
@@ -13,7 +12,67 @@ function SobreNos() {
     return (
         <React.Fragment>
         <Header />
-         
+        <div className="container py-5">
+            <h4 className="tl_title">Linha do Tempo</h4>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="main-timeline">
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <div className="circle"><img className="rounded-circle mb-5" src={TL_01} alt="Foto da Equipe" width="169" height="169"/></div>
+                                <div className="content">
+                                    <span className="year">1968-1992</span>
+                                    <h4 className="title">Inauguração</h4>
+                                    <p className="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lectus justo. Nam ultricies laoreet massa sed maximus. Curabitur tristique sagittis scelerisque. Vivamus posuere nisi quis tortor interdum, in finibus risus convallis. Suspendisse efficitur lacus a nulla commodo, sit amet vestibulum nibh.
+                                    </p>
+                                    <div className="icon"><span></span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <div className="circle"><img className="rounded-circle mb-5" src={TL_03} alt="Foto da Equipe" width="169" height="169"/></div>
+                                <div className="content">
+                                    <span className="year">1992-2006</span>
+                                    <h4 className="title">Reconhecido em todo solo Nacional</h4>
+                                    <p className="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lectus justo. Nam ultricies laoreet massa sed maximus. Curabitur tristique sagittis scelerisque. Vivamus posuere nisi quis tortor interdum, in finibus risus convallis. Suspendisse efficitur lacus a nulla commodo, sit amet vestibulum nibh.
+                                    </p>
+                                    <div className="icon"><span></span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <div className="circle"><img className="rounded-circle mb-5" src={TL_05} alt="Foto da Equipe" width="169" height="169"/></div>
+                                <div className="content">
+                                    <span className="year">2006-2011</span>
+                                    <h4 className="title">Milhares de Franquias sao espalhadas pelo mundo</h4>
+                                    <p className="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lectus justo. Nam ultricies laoreet massa sed maximus. Curabitur tristique sagittis scelerisque. Vivamus posuere nisi quis tortor interdum, in finibus risus convallis. Suspendisse efficitur lacus a nulla commodo, sit amet vestibulum nibh.
+                                    </p>
+                                    <div className="icon"><span></span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <div className="circle"><img className="rounded-circle mb-5" src={TL_04} alt="Foto da Equipe" width="169" height="169"/></div>
+                                <div className="content">
+                                    <span className="year">2011-2020</span>
+                                    <h4 className="title">Web Developer</h4>
+                                    <p className="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lectus justo. Nam ultricies laoreet massa sed maximus. Curabitur tristique sagittis scelerisque. Vivamus posuere nisi quis tortor interdum, in finibus risus convallis. Suspendisse efficitur lacus a nulla commodo, sit amet vestibulum nibh.
+                                    </p>
+                                    <div className="icon"><span></span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <Footer />
         </React.Fragment >
     )
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,7 +7,7 @@ import './styles/Header.css';
 function Header() {
     return (
         /*tras responsividade a nav em certos tipos de tamanho e alguns estilos*/
-            <nav className="navbar navbar-expand-xl navbar-dark bg-dark ">
+            <nav className="navbar navbar-expand-xl navbar-dark bg-dark">
                     
             
                     <NavLink to="" className="navbar-brand">Los Chefs</NavLink>
@@ -22,16 +22,16 @@ function Header() {
                                 <NavLink to="/" className="nav-link">Home</NavLink>
                             </li>
                             <li className="nav-item ">
-                                <NavLink to="/SeuPedido" className="nav-link ">Faca seu Pedido</NavLink>
+                                <NavLink to="/SeuPedido" className="nav-link " id="nav_item">Faca seu Pedido</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/Cardapio" className="nav-link">Reservas</NavLink>
+                                <NavLink to="/Cardapio" className="nav-link" id="nav_item">Reservas</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/Contatos" className="nav-link">Contatos</NavLink>
+                                <NavLink to="/Contatos" className="nav-link" id="nav_item">Contatos</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/SobreNos" className="nav-link">Sobre Nos</NavLink>
+                                <NavLink to="/SobreNos" className="nav-link" id="nav_item">Sobre Nos</NavLink>
                             </li>
                         </ul>
                     </div>
