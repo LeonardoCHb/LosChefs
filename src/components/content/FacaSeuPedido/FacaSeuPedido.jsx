7b726d61162293be815d57a747634d567060827f
caiolucasb <caio.lucas75@gmail.com>
2020-06-06 21:10:13 -0300

    finished jsx part, creating menuchoose food and dd

--- src/components/content/FacaSeuPedido/FacaSeuPedido.jsx
+++ src/components/content/FacaSeuPedido/FacaSeuPedido.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import '../styles/FacaSeuPedido.css'
 import Header from '../../layout/Header.jsx'
 import Footer from '../../layout/Footer'
-import MenuChoose from './FunctionsFacaSeuPedido'
+import MenuChooseFood from './MenuChooseFood'
+import MenuChooseDD from './MenuChooseDD'
 
 
 class SeuPedido extends React.Component {
@@ -16,67 +17,67 @@ class SeuPedido extends React.Component {
                             <li className="category-items list-group-item" >
                                 <ul className="category-item-01-pizza list-group">
                                     <h3>Pizzas</h3>
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Calabresa"
                                         text="Mozarela, linguiça calabresa artesanal, molho de tomates frescos e azeitonas pretas."
                                         preco="65.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="QuatroQueijos"
                                         text="Mussarela, requeijão, parmesão ralado, provolone."
                                         preco="60.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="FrangeComRequijão"
                                         text="Frango desfiado, cebola e requeijão"
                                         preco="65.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Margherita"
                                         text="Mussarela, tomate e manjericão."
                                         preco="55.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Pepperoni"
                                         text="Mussarela e pepperoni"
                                         preco="60.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Presunto"
                                         text="Mussarela e presunto"
                                         preco="50.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Napolitana"
                                         text="Mussarela, tomate e parmesão ralado."
                                         preco="55.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Américana"
                                         text="Mussarela, pepperoni, champignon, cebola e pimentão verde."
                                         preco="82.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="FrangoPhiladelphia"
                                         text="Mussarela, frango desfiado, Philadelphia e parmesão ralado."
                                         preco="85.70"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="Toscana"
                                         text="Mussarela, calabresa, requeijão, azeitona preta, cebola, tomate e manjericão."
                                         preco="75.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="p"
                                         name="DeLuxe"
                                         text="Mussarela, pepperoni, calabresa, champignon, cebola e pimentão verde."
@@ -87,37 +88,37 @@ class SeuPedido extends React.Component {
                             <li className="category-items list-group-item" >
                                 <ul className="category-item-02-lasagna list-group">
                                     <h3>lasanha(Lasagna)</h3>
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="Bolonhesa"
                                         text="Massa italiana com carne moída artesanal e um molho bolonhesa"
                                         preco="40.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="FrangoComRequeijão"
                                         text="Massa italiana com frango assado e um molho de requeijão com alho poró"
                                         preco="45.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="QuatroQueijos"
                                         text="Massa italiana com parmesão,mussarela,requeijão e provolone"
                                         preco="40.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="PeitoDePeru"
                                         text="Mozarela, linguiça calabresa artesanal, molho de tomates frescos e azeitonas pretas."
                                         preco="42.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="Calabresa"
                                         text="Massa italiana com finas fatias de calabresa artesanal,mussarela e parmesão"
                                         preco="42.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseFood
                                         type="l"
                                         name="CamarãoAoMolhoBranco"
                                         text="Massa italiana com cremoso molho camarões e alho poró, mussarela e parmesão."
@@ -128,31 +129,31 @@ class SeuPedido extends React.Component {
                             <li className="category-items list-group-item" >
                                 <ul className="category-item-04-drinks list-group">
                                     <h3>Bebidas(Drinks)</h3>
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="b"
                                         name="ÁguaNatural-300ml"
                                         text=""
                                         preco="7.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="b"
                                         name="ÁguaComGás-300ml"
                                         text=""
                                         preco="8.50"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="b"
                                         name="CastilloMurviedro-Espanha"
                                         text=""
                                         preco="75.00"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="b"
                                         name="Corinto-Chile"
                                         text=""
                                         preco="73.00"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="b"
                                         name="CatenaMalbec-2017"
                                         text=""
@@ -164,23 +165,23 @@ class SeuPedido extends React.Component {
                             <li className="category-items list-group-item" >
                                 <ul className="category-item-04-deserts list-group">
                                     <h3>Sobremesas(Deserts)</h3>
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="d"
                                         name="PetitGâteau"
                                         text=""
                                         preco="20.00"
                                     />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="d"
                                         name="MousseDeLimão"
                                         text=""
                                         preco="15.00" />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="d"
                                         name="CheeseCake"
                                         text=""
                                         preco="20.00" />
-                                    <MenuChoose
+                                    <MenuChooseDD
                                         type="d"
                                         name="TortaDeChocolate"
                                         text=""
---
+++ src/components/content/FacaSeuPedido/MenuChooseDD.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+
+class MenuChooseDD extends React.Component {
+    
+    render() {
+        return (
+            <React.Fragment>
+                <li className="each-item list-group-item d-flex justify-content-between w-100">
+                    <div className="d-flex div-item w-75">
+
+                        <div class="box">
+                            <span class="glyphicon glyphicon-ok-circle"></span>
+                            <svg id={"check-black-" + this.props.name} data-toggle="collapse" data-target={"." + this.props.name} class={this.props.name + " checks collapse bi bi-check-circle-fill"} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                            </svg>
+                            <svg id={"check-white-" + this.props.name} data-toggle="collapse" data-target={"." + this.props.name} class={this.props.name + " checks collapse show bi bi-check-circle"} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
+                            </svg>
+                        </div>
+
+                        <label className="">
+                            <h5 id={this.props.type + "-" + this.props.name}>{this.props.name}</h5>
+                            <small class="form-text text-muted">{this.props.text}</small>
+                        </label>
+                    </div>
+                    <div className="preco w-10 d-flex">
+                        <p className="text-align-center w-10">{this.props.preco}</p>
+                    </div>
+                </li>
+
+            </React.Fragment >)
+    }
+}
+
+
+export default MenuChooseDD
