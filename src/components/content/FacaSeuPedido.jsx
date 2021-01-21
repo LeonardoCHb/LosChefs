f24e590d929d7c327d070920426a8cfff0a11c41
caiolucasb <caio.lucas75@gmail.com>
2021-01-21 17:55:11 -0300

    Finishing franquias part

--- src/components/content/FacaSeuPedido.jsx
+++ src/components/content/FacaSeuPedido.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import './styles/FacaSeuPedido.css'
 import Header from '../layout/Header.jsx'
 import Footer from '../layout/Footer'
-
+import PizzaFranquia from './ReUse/PizzaFranquia'
 
 const SeuPedido = () => {
     return (
-        <div>
+        <div id="MainContainer">
             <Header/>
             <div id="MainContent">
                 <div id="informeSeuCep">
                     <p id="cepText">Informe seu CEP e faça o pedido na unidade mais proxíma</p>
                     <div id="buttonArea">
-                        <input></input>
+                        <input type="text"/>
                         <button>BUSCAR</button>
                     </div>
                     <a href="http://www.buscacep.correios.com.br/sistemas/buscacep/default.cfm" target="blank">Não sei meu CEP</a>
                 </div>
-                <div id="procurarFranaquia">procurarFranaquia</div>
+                <div id="procurarFranquias">
+                    <input id="porLocalInput" type="text" placeholder="Procure por cidade"/>
+                    <div id="displayFranquias">
+                        <PizzaFranquia Nome="AsaNorte" Endereço="SCHGN 702/703 Bloco G LOJA 47"/>
+                        <PizzaFranquia Nome="Cruzeiro" Endereço="SCHGN 702/703 Bloco G LOJA 47"/>
+                        <PizzaFranquia Nome="Sudoeste" Endereço="SCHGN 702/703 Bloco G LOJA 47"/>
+                        <PizzaFranquia Nome="Noroeste" Endereço="SCHGN 702/703 Bloco G LOJA 47"/>
+                    </div>
+                </div>
                 <div id="buttonsApp">buttonsApp</div>
             </div>
             
