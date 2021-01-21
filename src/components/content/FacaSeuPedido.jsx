27a0ed4c965c2a47c89c3d8b8e4e4025288b02ab
caiolucasb <caio.lucas75@gmail.com>
2021-01-21 14:08:02 -0300

    Doing the format and the styles of Cep part

--- src/components/content/FacaSeuPedido.jsx
+++ src/components/content/FacaSeuPedido.jsx
@@ -9,7 +9,14 @@ const SeuPedido = () => {
         <div>
             <Header/>
             <div id="MainContent">
-                <div id="informeSeuCep">informeSeuCep</div>
+                <div id="informeSeuCep">
+                    <p id="cepText">Informe seu CEP e faça o pedido na unidade mais proxíma</p>
+                    <div id="buttonArea">
+                        <input></input>
+                        <button>BUSCAR</button>
+                    </div>
+                    <a href="http://www.buscacep.correios.com.br/sistemas/buscacep/default.cfm" target="blank">Não sei meu CEP</a>
+                </div>
                 <div id="procurarFranaquia">procurarFranaquia</div>
                 <div id="buttonsApp">buttonsApp</div>
             </div>
