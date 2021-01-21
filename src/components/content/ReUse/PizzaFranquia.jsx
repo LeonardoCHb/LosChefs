bcef035e5cd40f001f59dd0f3b2446049f0bc505
caiolucasb <caio.lucas75@gmail.com>
2021-01-21 15:05:39 -0300

    Finishing pizzafranquia format and style

--- src/components/content/ReUse/PizzaFranquia.jsx
+++ src/components/content/ReUse/PizzaFranquia.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
-const PizzaFranquia = () => {
+import {BiTimeFive} from 'react-icons/bi'
+import {AiFillStar} from 'react-icons/ai'
+import './PizzaFranquia.css'
+import logo from '../../img/logo.png'
+const PizzaFranquia = ({Nome, Endereço}) => {
     return (
-        <div>
-            
+        <div id="Container">
+            <img id="logoFranquia" src={logo} alt="logo"/>
+            <div id="ContentDiv">
+                <h1>{`Los Chef's  -  ` + Nome}</h1>
+                <p>{Endereço}</p>
+                <div id="IconsPart">
+                    <div className="Icons">
+                        <AiFillStar/>
+                        <p>4.8</p>
+                    </div>
+                    <div className="Icons">
+                        <BiTimeFive/>
+                        <p>25-35 min</p>
+                    </div>
+                </div>
+                <p id="HorarioText">Aberto das 18:00 às 22:00</p>
+            </div>
         </div>
     )
 }
