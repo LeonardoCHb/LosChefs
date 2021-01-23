d6ff21b89912465ad9eff39204344e84e3e02959
caiolucasb <caio.lucas75@gmail.com>
2021-01-23 03:01:56 -0300

    beginning of reservas part

--- src/components/content/Reservas.jsx
+++ src/components/content/Reservas.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
+import './styles/Reservas.css'
 
+import Header from '../layout/Header'
+import Footer from '../layout/Footer'
 
-class Reservas extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            name: "1"
-        }
-    }
-
-    render(){
-        return (<h1>Omoshiroi {this.state.name}</h1>)
-    }
+const Reservas = () => {
+    return (
+        <div>
+            <Header/>
+            <div id="MainContent">
+                <div id="imgAndText"></div>
+                <div id="FormPart">
+                    <h1>Escolha a unidade que você deseja fazer a sua pré-reserva</h1>
+                    <form action="post" id="MainForm">
+                        <label className="InputField">
+                            Nome Completo:
+                             <input type="text"/>
+                        </label>
+                        <label className="InputField">
+                            E-mail:
+                             <input type="E-mail"/>
+                        </label>
+                        <label className="InputField">
+                            Quantidade de pessoas:
+                             <input type="number"/>
+                        </label>
+                        <label className="InputField">
+                            Telefone para contato:
+                             <input type="tel"/>
+                        </label>
+                        <label className="InputField">
+                            Data da Reserva:
+                             <input type="date"/>
+                        </label>
+                        <button>FAÇA A SUA PRÉ-RESERVA</button>
+                    </form>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    )
 }
 
 export default Reservas
