ee4d2f32620243d82f7693aa15dc9ef1ef217b52
caiolucasb <caio.lucas75@gmail.com>
2021-03-11 19:35:13 -0300

    Creating Array function and changing storage keys

--- src/Storage/LocalStorage.js
+++ src/Storage/LocalStorage.js
@@ -1,40 +1,33 @@
-function CreatePizzaria(nome,endereço,estrelas){
+function CreatePizzaria(nome,cep,endereco,estrelas){
     const Pizzaria = {
         Nome: nome,
-        Endereço: endereço,
+        Cep: cep,
+        Endereco: endereco,
         Estrelas: estrelas
     }
     return Pizzaria;
 }
 
-const PizzaAsaNorte = CreatePizzaria('Asa Norte','Quadra CLN 210 Bloco A', 4.5);//70852510
-const PizzaAsaSul = CreatePizzaria('Asa Sul', 'SBS Quadra 1 BLoco A Lote 31', 4.6);//70073-900
-const PizzaLogoNorte = CreatePizzaria('Lago Norte', 'Quadra SMLN ML Trecho 6', 4.8); //71540060
-const PizzaLagoSul = CreatePizzaria('Lago Sul', 'Condomínio do Lago Sul (St Hab Dom Bosco)', 4.8);//71676250
-const PizzaNoroeste = CreatePizzaria('Noroeste', 'Quadra CRNW 506 Bloco A Lote 2', 4.7);//70684510
-const PizzaSudoeste = CreatePizzaria('Sudoeste', 'SQSW 100 Bloco F', 4.7);//70670016
-export const PizzariasQueue = [{Nome:"Asa Norte",Cep:70852510},{Nome:"Asa Sul",Cep:70073900},{Nome:"Lago Norte",Cep:71540060},{Nome:"Lago Sul",Cep:71676250},{Nome:"Noroeste",Cep:70684510},{Nome:"Sudoeste",Cep:70670016}]
+const PizzaAsaNorte = CreatePizzaria('Asa Norte',"70852510",'Quadra CLN 210 Bloco A', 4.5);//70852510
+const PizzaAsaSul = CreatePizzaria('Asa Sul',"70073900", 'SBS Quadra 1 BLoco A Lote 31', 4.6);//70073-900
+const PizzaLogoNorte = CreatePizzaria('Lago Norte',"71540060", 'Quadra SMLN ML Trecho 6', 4.8); //71540060
+const PizzaLagoSul = CreatePizzaria('Lago Sul',"71676250",'Condomínio do Lago Sul (St Hab Dom Bosco)', 4.8);//71676250
+const PizzaNoroeste = CreatePizzaria('Noroeste',"70684510", 'Quadra CRNW 506 Bloco A Lote 2', 4.7);//70684510
+const PizzaSudoeste = CreatePizzaria('Sudoeste',"70670016", 'SQSW 100 Bloco F', 4.7);//70670016
 
 export default function SetLocalStorage(){
-    localStorage.setItem('70852510', JSON.stringify(PizzaAsaNorte))
-    localStorage.setItem('70073900', JSON.stringify(PizzaAsaSul))
-    localStorage.setItem('71540060', JSON.stringify(PizzaLogoNorte))
-    localStorage.setItem('71676250', JSON.stringify(PizzaLagoSul))
-    localStorage.setItem('70684510', JSON.stringify(PizzaNoroeste))
-    localStorage.setItem('70670016', JSON.stringify(PizzaSudoeste))
-}
-export function GetLocalStorage(Cep){
-    const LocalItem = localStorage.getItem(Cep);
-    return JSON.parse(LocalItem);
-}
-export function SearchByLocation(Input){
-    const input = String.prototype.toLowerCase(Input)
-    PizzariasQueue.map(obj => {
-        const Nome = String.prototype.toLowerCase(obj.nome)
-        if(Nome === input)
-            return GetLocalStorage(obj.Cep)
-    })
-
+    localStorage.setItem('0', JSON.stringify(PizzaAsaNorte))
+    localStorage.setItem('1', JSON.stringify(PizzaAsaSul))
+    localStorage.setItem('2', JSON.stringify(PizzaLogoNorte))
+    localStorage.setItem('3', JSON.stringify(PizzaLagoSul))
+    localStorage.setItem('4', JSON.stringify(PizzaNoroeste))
+    localStorage.setItem('5', JSON.stringify(PizzaSudoeste))
 }
 
-
+export function ArrayOfData(){
+    var Array = [];
+    for(let i = 0; i < localStorage.length; i++){
+        Array.push(JSON.parse(localStorage.getItem(JSON.stringify(i))))
+    }
+    return Array;
+}
